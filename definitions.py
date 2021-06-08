0c514d7579d8e3abc526dd69622eff52e6f58548
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-06-08 10:36:38 -0400

    adding README.md

--- definitions.py
+++ definitions.py
@@ -3,5 +3,5 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 CALIB_PERIOD = 1.5  # year (from Mar-1, 2020 to Aug-31, 2021 which is 1.5 years)
-PROJ_PERIOD = 0.75  # year (from Sep-1, 2021 to May-31, 2021 whic is 0.75 year)
+PROJ_PERIOD = 0.75  # year (from Sep-1, 2021 to May-31, 2022 which is 0.75 year)
 SIM_DURATION = CALIB_PERIOD + PROJ_PERIOD
