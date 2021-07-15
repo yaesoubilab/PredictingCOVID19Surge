5afef22766b56199467888aed82825c86eb58571
shiying88 <shiying.you@yale.edu>
2021-07-15 15:03:42 -0400

    minor updates

--- Predict.py
+++ Predict.py
@@ -4,10 +4,6 @@ from covid_prediction.prediction_models import *
 
 df = pd.read_csv('outputs/prediction_dataset/cleaned_data.csv')
 
-# TODO: see my comments on feature_engineering.py first.
-#   Since we are also interested in predicting the maximum rate of hospitalization, would you
-#   also add a linear regression model too?
-
 feature_names = ['Obs: Incidence', 'Obs: Cumulative vaccination']
 y_name_binary = 'if_surpass'
 y_name_rate = 'maximum_occupancy'
