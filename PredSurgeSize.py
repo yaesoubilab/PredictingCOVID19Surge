cffd654322c2cde80c78f90b0977166a19fb66c8
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-07-30 09:51:40 -0400

    recalibrated + dataset

---
+++ PredSurgeSize.py
@@ -0,0 +1,1 @@
+# use the entire dataset to predict the size of surge using logistic regression and neural network
---
+++ PredictSurgeProb.py
@@ -0,0 +1,1 @@
+# use the entire dataset to predict the probability of surge using logistic regression and neural network
