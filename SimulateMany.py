81ee7facea2ccafb1f95235ca2e40de0b3976f6c
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-08-28 14:59:52 -0400

    display a random number of trajs

--- SimulateMany.py
+++ SimulateMany.py
@@ -5,11 +5,12 @@ from covid_model.settings import COVIDSettings
 from covid_visualization.plot_trajs import plot
 
 N = 50   # number of trajectories to simulate
+N_TO_DISPLAY = 25 # number of trajectories to display
 IF_PARALLEL = True
 USE_CALIBRATED_MODEL = True
 
 
-def simulate(n=25, calibrated=True, seeds=None, weights=None, sample_seeds_by_weights=False):
+def simulate(n=25, n_to_display=None, calibrated=True, seeds=None, weights=None, sample_seeds_by_weights=False):
 
     # get model settings
     sets = COVIDSettings()
@@ -35,14 +36,14 @@ def simulate(n=25, calibrated=True, seeds=None, weights=None, sample_seeds_by_we
     multi_model.print_summary_stats()
 
     # plot trajectories
-    if n < 200:
-        plot(prev_multiplier=52,  # to show weeks on the x-axis of prevalence data
-             incd_multiplier=sets.simulationOutputPeriod * 52, # to show weeks on the x-axis of incidence data
-             obs_incd_multiplier=sets.observationPeriod*52
-             )
+    plot(prev_multiplier=52,  # to show weeks on the x-axis of prevalence data
+         incd_multiplier=sets.simulationOutputPeriod * 52,  # to show weeks on the x-axis of incidence data
+         obs_incd_multiplier=sets.observationPeriod*52,
+         n_random_trajs_to_display=n_to_display
+         )
 
 
 if __name__ == "__main__":
 
-    simulate(n=N, calibrated=USE_CALIBRATED_MODEL)
+    simulate(n=N, n_to_display=N_TO_DISPLAY, calibrated=USE_CALIBRATED_MODEL)
 
--- covid_visualization/plot_trajs.py
+++ covid_visualization/plot_trajs.py
@@ -6,16 +6,19 @@ from covid_model.data import MAX_HOSP_RATE_OVERALL, MIN_HOSP_RATE_OVERALL, VACCI
 from covid_model.settings import COVIDSettings
 from definitions import AgeGroups, Profiles, FEASIBILITY_PERIOD, ROOT_DIR
 
+
 A.FEASIBLE_REGION_COLOR_CODE = 'pink'
 IF_MAKE_VALIDATION_PLOTS = False
 A.Y_LABEL_COORD_X = -0.15
 A.SUBPLOT_W_SPACE = 0.0
 
 
-def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
+def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1, n_random_trajs_to_display=None):
     """
     :param prev_multiplier: (int) to multiply the simulation time to convert it to year, week, or day.
     :param incd_multiplier: (int) to multiply the simulation period to covert it to year, week, or day.
+    :param obs_incd_multiplier: (int) to multiply the observation period to conver it to year, week, or day.
+    :param n_random_trajs_to_display: (int) number of trajectories to display
     :return:
     """
 
@@ -66,6 +69,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
                                           list_plot_info=[Es[0], Is[0], Hs[0], Rs[0], Rs[0],
                                                           Es[1], Is[1], Hs[1], Rs[1], Rs[1],
                                                           S, V],
+                                          n_random_trajs_to_display=n_random_trajs_to_display,
                                           file_name=filename_validation,
                                           figure_size=(11, 5.5))
 
@@ -109,6 +113,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
                                   list_plot_info=[obs_hosp_rate, obs_cum_hosp_rate,
                                                   obs_cum_vacc_rate],
                                   file_name=ROOT_DIR+'/outputs/figures/summary3.png',
+                                  n_random_trajs_to_display=n_random_trajs_to_display,
                                   show_subplot_labels=True,
                                   figure_size=(2.3*3, 2.4)
                                   )
@@ -116,6 +121,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
                                   list_plot_info=[obs_hosp_rate, obs_cum_hosp_rate,
                                                   obs_cum_vacc_rate, obs_incd_novel],
                                   file_name=ROOT_DIR+'/outputs/figures/summary.png',
+                                  n_random_trajs_to_display=n_random_trajs_to_display,
                                   show_subplot_labels=True,
                                   figure_size=(2.3*2, 2.4*2)
                                   )
@@ -167,6 +173,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
                                   list_plot_info=[obs_incd_novel, obs_incd_novel_unvacc, obs_incd_novl_vacc,
                                                   obs_new_hosp_novel, obs_new_hosp_novel_unvacc, obs_new_hosp_novel_vacc],
                                   file_name=ROOT_DIR+'/outputs/figures/novel_variant.png',
+                                  n_random_trajs_to_display=n_random_trajs_to_display,
                                   show_subplot_labels=True,
                                   figure_size=(2.3*3, 2.4*2)
                                   )
@@ -219,6 +226,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
     A.Y_LABEL_COORD_X = -0.35
     sim_outcomes.plot_multi_panel(n_rows=4, n_cols=len(AgeGroups),
                                   list_plot_info=list_plot_info,
+                                  n_random_trajs_to_display=n_random_trajs_to_display,
                                   file_name=filename_validation,
                                   figure_size=(11, 6.5))
 
@@ -259,6 +267,7 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1):
     A.Y_LABEL_COORD_X = -0.25
     sim_outcomes.plot_multi_panel(n_rows=2, n_cols=len(AgeGroups),
                                   list_plot_info=list_plot_info,
+                                  n_random_trajs_to_display=n_random_trajs_to_display,
                                   file_name=filename_validation,
                                   figure_size=(15, 4))
 
@@ -270,5 +279,5 @@ if __name__ == "__main__":
 
     plot(prev_multiplier=52,  # to show weeks on the x-axis of prevalence data
          incd_multiplier=sets.simulationOutputPeriod * 52,  # to show weeks on the x-axis of incidence data
-         obs_incd_multiplier=sets.observationPeriod * 52
+         obs_incd_multiplier=sets.observationPeriod * 52,
          )
