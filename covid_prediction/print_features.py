cb35ff11a99554d3137c8a16afefa466f091ccc7
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-08-29 14:53:43 -0400

    replacing feature names

--- covid_prediction/print_features.py
+++ covid_prediction/print_features.py
@@ -9,13 +9,25 @@ MODELS = (Zero, A, B, C, D)
 def print_selected_features():
 
     # read feature names
-    rows = read_csv_rows(file_name=ROOT_DIR+'/outputs/prediction_datasets/features.csv',
-                         if_ignore_first_row=False)
+    feature_names = read_csv_rows(
+        file_name=ROOT_DIR+'/outputs/prediction_datasets/features.csv',
+        if_ignore_first_row=False)
+    feature_names = [f[0] for f in feature_names]
 
-    # make a dictionary with all features as key
-    dic_of_all_features = {}
-    for r in rows:
-        dic_of_all_features[r[0]] = [None] * len(WEEKS) * len(MODELS)
+    # read original feature names and cleaned feature names
+    f_names_and_cleaned_names = read_csv_rows(
+        file_name=ROOT_DIR + '/covid_prediction/alternative feature names.csv',
+        if_ignore_first_row=False)
+
+    # make a dictionary of cleaned name with original name as keys
+    dict_cleaned_feature_names = {}
+    for row in f_names_and_cleaned_names:
+        dict_cleaned_feature_names[row[0]] = row[1]
+
+    # make a dictionary with all (cleaned) feature names as key
+    dic_cleaned_features_and_selection_results = {}
+    for r, v in dict_cleaned_feature_names.items():
+        dic_cleaned_features_and_selection_results[v] = [None] * len(WEEKS) * len(MODELS)
 
     # read all files with selected features
     i = 0
@@ -33,27 +45,29 @@ def print_selected_features():
             # in the list of all features, mark the features selected in this model
             if model.features is not None:
                 for f in model.features:
+                    cleaned_f = dict_cleaned_feature_names[f]
                     if f in selected_features:
-                        dic_of_all_features[f][i] = '+'
+                        dic_cleaned_features_and_selection_results[cleaned_f][i] = '+'
                     else:
-                        dic_of_all_features[f][i] = '-'
+                        dic_cleaned_features_and_selection_results[cleaned_f][i] = '-'
             else:
-                for f in dic_of_all_features:
+                for f in feature_names:
+                    cleaned_f = dict_cleaned_feature_names[f]
                     if f in selected_features:
-                        dic_of_all_features[f][i] = '+'
+                        dic_cleaned_features_and_selection_results[cleaned_f][i] = '+'
                     else:
-                        dic_of_all_features[f][i] = '-'
+                        dic_cleaned_features_and_selection_results[cleaned_f][i] = '-'
 
             i += 1
 
     # print results
-    rows = [col_names]
-    for f, v in dic_of_all_features.items():
+    result = [col_names]
+    for f, v in dic_cleaned_features_and_selection_results.items():
         row = [f]
         row.extend(v)
-        rows.append(row)
+        result.append(row)
 
-    write_csv(rows=rows, file_name=ROOT_DIR+'/outputs/prediction_summary/selected_features.csv')
+    write_csv(rows=result, file_name=ROOT_DIR+'/outputs/prediction_summary/selected_features.csv')
 
 
 if __name__ == '__main__':
