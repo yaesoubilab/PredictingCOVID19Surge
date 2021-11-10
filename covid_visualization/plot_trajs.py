d75dcb6a4a24687647d85859d718dab87621dc33
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-11-10 15:38:09 -0500

    working on plots

--- covid_visualization/plot_trajs.py
+++ covid_visualization/plot_trajs.py
@@ -156,10 +156,13 @@ def plot(prev_multiplier=52, incd_multiplier=1, obs_incd_multiplier=1,
     # -----------------------------------------------------
     # ------ plot information for the novel variant plot --------
     # -----------------------------------------------------
+    # % incidence that are vaccinated
+    # % hospitalizations that are vaccinated
+
     # % incidence due to delta
     # % hospitalizations due to delta
-    # % incidence that are unvaccinated and due to delta
-    # % hospitalizations that are unvaccinated and due to delta
+    # % incidence that are vaccinated and due to delta
+    # % hospitalizations that are vaccinated and due to delta
 
     obs_incd_delta = A.TrajPlotInfo(
         outcome_name='Obs: % of incidence due to Delta',
